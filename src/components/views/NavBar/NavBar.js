716c1bca5931568a2ba182aef991f1a9ca7ac0a4
Maria Gozdawa-Gołębiowska <m.gozdawa-golebiowska@gmail.com>
2024-07-02 14:29:00 +0200

    add styles for NavBar

--- src/components/views/NavBar/NavBar.js
+++ src/components/views/NavBar/NavBar.js
@@ -1,20 +1,22 @@
-import { Container, Nav, Navbar, NavbarBrand } from 'react-bootstrap';
-import styles from './NavBar.module.scss'
+import React from 'react';
+import { Navbar, Container, Nav, NavbarBrand, NavDropdown } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import styles from './NavBar.module.scss';
 
 const NavBar = () => {
-    return (
-        <section>
-            <Navbar bg='primary' variant='dark' expand='lg' className='mt-4 mb-4 rounded'>
-                <Container className={styles.nav}>
-                    <NavbarBrand as={NavLink} to="/">NavBar</NavbarBrand>
-                    <Nav className='me-auto'>
-                        <Nav.Link as={NavLink} to='/'>Home</Nav.Link>
-                        <Nav.Link as={NavLink} to='/about'>About</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
-        </section>
-    )
-}
+  return (
+    <section>
+      <Navbar bg='primary' variant='dark' expand='lg' className='mt-4 mb-4 rounded'>
+        <Container className={styles.nav}>
+          <NavbarBrand as={NavLink} to="/">Blog.app</NavbarBrand>
+          <Nav className={styles['nav-links']}>
+            <Nav.Link as={NavLink} to='/about'>About</Nav.Link>
+            <Nav.Link as={NavLink} to='/'>Home</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </section>
+  );
+};
+
 export default NavBar;
